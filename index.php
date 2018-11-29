5e52359b7762047d8bfffdfdd7c66e778860b46f
Andrey Sushentsov <p1oneer8527@gmail.com>
2018-11-29 23:35:52 +0300

    loop on front-page, styled layout

--- index.php
+++ index.php
@@ -24,5 +24,73 @@
 			) );?>
 		</nav>
 	</header>
+	<main class="container">
+		<div class="page__title">Свежие посты в Back End</div>
+
+		<div class="post-card__container">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+			/* 
+			 * Выводит посты из категории Back End
+			 */
+			if ( in_category( 4 ) ) : ?>
+			 
+				<div class="post-card">
+			        <h2><?php the_title() ?></h2>
+			 
+			        <small><?php _e( 'Автор ', 'textdomain' ); the_author_posts_link() ?></small>
+
+			 		<div class="entry">
+			            <?php the_content() ?>
+			        </div>
+			
+			        <?php _e( 'Категория: ', 'textdomain' ); the_category( ', ' ); ?>
+			
+				</div>
+			<?php endif; ?>
+			<?php endwhile; else :
+			/*
+			 * Если постов в данной категории не найден
+			 */
+			_e( 'Извените, но пока в данной категории нет ни одного поста. ((', 'textdomain' );
+			 // Завершить цикл
+			 endif;
+			 // Сбросить данные
+			 rewind_posts(); 
+			?>
+		</div>
+
+		<div class="page__title">Свежие посты в Front End</div>
+
+		<div class="post-card__container">
+			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+			/* 
+			 * Выводит посты из категории Font End  
+			 */
+			if ( in_category( 3 ) ) : ?>
+			 
+				<div class="post-card">
+			        <h2><?php the_title() ?></h2>
+			 
+			        <small><?php _e( 'Автор: ', 'textdomain' ); the_author_posts_link() ?></small>
+
+			 		<div class="entry">
+			            <?php the_content() ?>
+			        </div>
+			
+			        <?php _e( 'Категория ', 'textdomain' ); the_category( ', ' ); ?>
+			
+				</div>
+			<?php endif; ?>
+			<?php endwhile; else :
+			/*
+			 * Если в данной категории нет постов
+			 */
+			_e( 'Извените на данный момент в этой категории нет постов. ((', 'textdomain' );
+			 // Завершить цикл
+			 endif;
+			?>			
+		</div>
+
+	</main>
 </body>
 </html>
