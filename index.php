a209deab89818cdd2808b3196429381de1993dfe
Andrey Sushentsov <p1oneer8527@gmail.com>
2018-12-02 00:31:30 +0300

    categoryes loop

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php get_header();?>
-	index
+	
+<?php	
+ // вместо "5" указываем идентификатор вашей рубрики.
+ 	while (have_posts()) : the_post();?>
+ 
+<!--в этой строке выводится название статьи и ссылка на неё-->
+	<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+
+<!--здесь выводится миниатюра записи-->
+	<div class="thumbnail"><?php echo get_the_post_thumbnail(); ?></div>
+	<?php the_content(); // вывод текста записи
+	endwhile;
+	wp_reset_query();
+?>
 <?php get_footer();?>
